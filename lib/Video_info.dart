b153a97f153c1b56179eddef86cb2307a2605ee8
siam786 <shohrab.hossain36@gmail.com>
2022-02-16 23:46:31 +0600

    add video page

--- lib/Video_info.dart
+++ lib/Video_info.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 import 'colors.dart' as color;
 
 class Video_info extends StatefulWidget {
@@ -17,10 +18,34 @@ class _Video_infoState extends State<Video_info> {
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
-              color.AppColor.gradientFirst,
+              color.AppColor.gradientFirst.withOpacity(0.9),
               color.AppColor.gradientSecond
-            ]
+            ],
+            begin: const FractionalOffset(0.0, 0.9),
+            end: Alignment.topRight
           )
+        ),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 70,left: 30,right: 30),
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              child: Column(
+                children: [
+                 Row(
+                    children: [
+                      Icon(Icons.arrow_back_ios,size: 20,
+                  color: color.AppColor.secondPageIconColor),
+                  Expanded(child: Container()),
+                  Icon(Icons.info_outline,size: 20,
+                  color: color.AppColor.secondPageIconColor),
+                    ],
+                 )
+                ],
+              ),
+            )
+          ],
         ),
       ),
       
