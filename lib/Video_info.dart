d64135170781985ff05352e6512d7716d23d8aae
siam786 <shohrab.hossain36@gmail.com>
2022-02-17 07:36:36 +0600

    add video page

--- lib/Video_info.dart
+++ lib/Video_info.dart
@@ -1,10 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'colors.dart' as color;
 
 class Video_info extends StatefulWidget {
-  const Video_info({ Key? key }) : super(key: key);
+  const Video_info({Key? key}) : super(key: key);
 
   @override
   _Video_infoState createState() => _Video_infoState();
@@ -16,39 +18,154 @@ class _Video_infoState extends State<Video_info> {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              color.AppColor.gradientFirst.withOpacity(0.9),
-              color.AppColor.gradientSecond
-            ],
-            begin: const FractionalOffset(0.0, 0.9),
-            end: Alignment.topRight
-          )
-        ),
+            gradient: LinearGradient(colors: [
+          color.AppColor.gradientFirst.withOpacity(0.9),
+          color.AppColor.gradientSecond
+        ], begin: const FractionalOffset(0.0, 0.9), end: Alignment.topRight)),
         child: Column(
           children: [
             Container(
-              padding: const EdgeInsets.only(top: 70,left: 30,right: 30),
+              padding: const EdgeInsets.only(top: 70, left: 30, right: 30),
               width: MediaQuery.of(context).size.width,
               height: 300,
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                 Row(
+                  Row(
                     children: [
-                      Icon(Icons.arrow_back_ios,size: 20,
-                  color: color.AppColor.secondPageIconColor),
-                  Expanded(child: Container()),
-                  Icon(Icons.info_outline,size: 20,
-                  color: color.AppColor.secondPageIconColor),
+                      Icon(Icons.arrow_back_ios,
+                          size: 20, color: color.AppColor.secondPageIconColor),
+                      Expanded(child: Container()),
+                      Icon(Icons.info_outline,
+                          size: 20, color: color.AppColor.secondPageIconColor),
                     ],
-                 )
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    "Leg Toning",
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: color.AppColor.secondPageTitleColor,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "and Glutes Workout",
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: color.AppColor.secondPageTitleColor),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 90,
+                        height: 30,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                                colors: [
+                                  color.AppColor
+                                      .secondPageContainerGradient1stColor,
+                                  color.AppColor
+                                      .secondPageContainerGradient2ndColor
+                                ],
+                                begin: Alignment.bottomLeft,
+                                end: Alignment.topRight)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.timer,
+                              size: 20,
+                              color: color.AppColor.secondPageIconColor,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              "68 min",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: color.AppColor.secondPageIconColor),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        width: 250,
+                        height: 30,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                                colors: [
+                                  color.AppColor
+                                      .secondPageContainerGradient1stColor,
+                                  color.AppColor
+                                      .secondPageContainerGradient2ndColor
+                                ],
+                                begin: Alignment.bottomLeft,
+                                end: Alignment.topRight)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.handyman_outlined,
+                              size: 20,
+                              color: color.AppColor.secondPageIconColor,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              "Registance band,kettebell",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: color.AppColor.secondPageIconColor),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
-            )
+            ),
+            Expanded(
+                child: Container(
+                decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(70)
+                ),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: 30,),
+                  Row(
+                    children: [
+                      Text("Circuit 1: Legs Toning",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: color.AppColor.circuitsColor
+                      ),)
+                    ],
+                  )
+              ],),
+            ))
           ],
         ),
       ),
-      
     );
   }
 }
